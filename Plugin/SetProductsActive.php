707a1a4ea42a15519de3c34a869ec6e48ef7bcb1
Kevin Meijer <kevin@justbetter.nl>
2021-07-06 14:07:53 +0200

    FUL-60 - Add option to enable all products from import

---
+++ Plugin/SetProductsActive.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace JustBetter\AkeneoBundle\Plugin;
+
+use Akeneo\Connector\Job\Product;
+use Akeneo\Connector\Helper\Import\Entities;
+use Magento\Store\Model\ScopeInterface as scope;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class SetProductsActive
+{
+    protected $config;
+    protected $entitiesHelper;
+
+    /**
+     * __construct function
+     * @param ScopeConfigInterface $config
+     * @param Entities $entitiesHelper
+     */
+    public function __construct(
+        ScopeConfigInterface $config,
+        Entities $entitiesHelper
+    ) {
+        $this->config = $config;
+        $this->entitiesHelper = $entitiesHelper;
+    }
+
+    /**
+     * afterInsertData function
+     * @param  product $subject
+     * @param  bool $result
+     * @return bool $result
+     */
+    public function afterInsertData(product $subject, $result)
+    {
+        $extensionEnabled = $this->config->getValue('akeneo_connector/justbetter/setproductsactive', scope::SCOPE_WEBSITE);
+        if (!$extensionEnabled) {
+            return $result;
+        }
+
+        // todo:
+
+        return $result;
+    }
+}
