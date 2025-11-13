18b41959d9cd0747a2311d322eb61a52eb38257d
Claudio Ferraro <claudio@justbetter.nl>
2025-11-13 15:39:50 +0100

    Fix: update $storeId type hints to int|string|null in SlackHelper
    Changed the $storeId parameters in Helper/SlackHelper.php from ?int to int|string|null to ensure full compatibility with Magento's ScopeConfigInterface.

--- Helper/SlackHelper.php
+++ Helper/SlackHelper.php
@@ -14,7 +14,7 @@ class SlackHelper extends AbstractHelper
      * @param  String $field
      * @param  Int $storeId
      */
-    public function getConfigValue($field, ?int $storeId = null)
+    public function getConfigValue($field, int|string|null $storeId = null)
     {
         return $this->scopeConfig->getValue(
             $field,
@@ -28,7 +28,7 @@ class SlackHelper extends AbstractHelper
      * @param  String $code
      * @param  null $storeId
      */
-    public function getGeneralConfig($code, ?int $storeId = null)
+    public function getGeneralConfig($code, int|string|null $storeId = null)
     {
         return $this->getConfigValue(self::XML_PATH . $code, $storeId);
     }
